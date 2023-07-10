565baa3b8b3c30573ddc4da4c83e70e28300f929
Nader Abdulrub <nabdulrub16@gmail.com>
2023-07-10 18:14:27 -0400

    port change

--- server/server.js
+++ server/server.js
@@ -47,8 +47,10 @@ app.post("/", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log("Server listening on port " + PORT);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
